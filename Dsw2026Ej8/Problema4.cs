aa3eeeb39d377c97b71402fb45686c8d8a443036
Felipe <Felipe.Cattolica@alu.frt.utn.edu.ar>
2026-05-03 06:55:03 -0300

    Resolucion problema 4

---
+++ Dsw2026Ej8/Problema4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema4
+    {
+        public double CalcularPromedio(int? nota1, int? nota2, int? nota3)
+        {
+
+            int suma = 0;
+            int contador = 0;
+
+            int?[] notas = {nota1, nota2, nota3};
+
+            foreach (var nota in notas)
+            {
+
+                if(nota.HasValue && nota.Value >= 0 && nota.Value <= 10)
+                {
+                    contador++;
+                    suma += nota.Value;
+
+
+                }
+
+            }
+            return contador > 0 ? (double)suma / contador : 0;
+        }
+
+
+    }
+}
