99991c390ab4ce4654d26bfb64e8e12a4512ab18
Felipe <Felipe.Cattolica@alu.frt.utn.edu.ar>
2026-05-03 06:51:51 -0300

    Resolucion problema 3

---
+++ Dsw2026Ej8/Problema3.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Dsw2026Ej8
+{
+    public class Problema3
+    {
+
+        public string CompararCopias(int originalValue, Product product)
+        {
+            int valueCopy = originalValue;
+            valueCopy++;
+
+            Product productCopy = product;
+            productCopy.Update("Descripcion modificada");
+
+            return $"{originalValue}-{valueCopy}-{productCopy.GetDescription}";
+            
+
+
+
+
+
+        }
+
+
+    }
+}
