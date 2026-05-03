7a4c3edb9dd29f81728e1134d7b02b4ed86dc1ec
Felipe <Felipe.Cattolica@alu.frt.utn.edu.ar>
2026-05-03 05:49:37 -0300

    resolución problema 2

--- Dsw2026Ej8/Problema2.cs
+++ Dsw2026Ej8/Problema2.cs
@@ -1,10 +1,31 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Numerics;
 using System.Text;
 
 namespace Dsw2026Ej8
 {
     internal class Problema2
     {
+
+        public string CrearResumenVenta(long productCode, string productDescription, int quantity, decimal unitPrice) {
+
+            decimal total = quantity * unitPrice;
+
+
+            if (quantity <= 0) {
+
+                total = 0;
+
+            }
+
+            var anon = new(productCode, productDescription, quantity, unitPrice);
+
+
+
+            return null
+        }
+
+
     }
 }
